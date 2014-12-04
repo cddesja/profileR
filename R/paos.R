7eda354a199ad1401aa453723c23df58003b6b01
Okan Bulut <bulut@ualberta.ca>
2014-12-04 10:19:29 -0700

    paos function has been added.

--- R/paos.R
+++ R/paos.R
@@ -1,35 +1,67 @@
 
-#Profile Analysis for One Sample with Hotelling's T-Square
-paos <- function(data, mu0=NULL) {
-  if(is.null(mu0)) {mu0=1} else
-  {mu0=mu0}
+#'@export
+
+paos <- function(data, scale=TRUE) {
   
-  data <- as.matrix(data)
-  i=ncol(data)
-  j=nrow(data)
+  if(scale){
+    x1 <- apply(data,2,function(x) scale(x, center = FALSE, scale = TRUE))
+    x1 <- as.matrix(x1, ncol=ncol(data))
+  } else 
+    {x1 <- as.matrix(data, ncol=ncol(data))}
   
-  m <- rep(mu0,i)
-  one <- matrix(1,nrow=j,ncol=1)
-  ident <- diag(j)
-  ybar <- t(data)%*%one/j
-  s <- (t(data)%*%(ident-one%*%t(one)/j))%*%(data/(j-1))
+  #The difference matrix for Part II
+  x2 <- matrix(, nrow=nrow(data), ncol=(ncol(data)-1))
   
-  #ybar <- as.matrix(colMeans(data, na.rm=TRUE),nrow=i,ncol=1)
-  #s <- cov(data)
+  for (i in 1:(ncol(x1)-1)) {
+    x2[,i] <- x1[,(i+1)]-x1[,i]
+  }
+
   
-  t2 <- j*t(ybar-mu0)%*%solve(s)%*%(ybar-mu0)
-  f <- (j-i)*t2/i/(j-1)
-  df1=i
-  df2=j-i
-  p <- 1-pf(f, df1, df2) 
-  ftable <- data.frame(t2,f,df1,df2,p)
-  colnames(ftable) <- c("T-Squared","F","df1","df2","p-value")
-  rownames(ftable) <- c("Ho: Ratios of the means=Mu0")
+  #PART I
+  mu0 <-as.matrix(rep(1,ncol(x1)),ncol=1)
+  one <- as.matrix(rep(1,nrow(x1)),ncol=1)
+  ident <- diag(nrow(x1))
+  ybar <- t(x1)%*%one/nrow(x1)
+  s <- t(x1)%*%(ident-one%*%t(one)/nrow(x1))%*%x/(nrow(x1)-1)
+
+#   print(mu0)
+#   print(ybar)
+#   print(s)
+
+  t2 <- nrow(x)%*%t(ybar-mu0)%*%solve(s)%*%(ybar-mu0)
+  f <- (nrow(x)-ncol(x))*t2/ncol(x)/(nrow(x)-1)
+  df1=ncol(x)
+  df2=nrow(x)-ncol(x)
+  p <- 1-pf(f, df1, df2, lower.tail = TRUE, log.p = FALSE)
+  result1 <- data.frame(t2=t2,F=f,df1=df1,df2=df2,p.value=p);
+
+
+  #PART II
+  mu0 <-as.matrix(rep(0,ncol(x2)),ncol=1)
+  one <- as.matrix(rep(1,nrow(x2)),ncol=1)
+  ident <- diag(nrow(x2))
+  ybar <- t(x2)%*%one/nrow(x2)
+  s <- t(x2)%*%(ident-one%*%t(one)/nrow(x2))%*%x2/(nrow(x2)-1)
+
+#   print(mu0)
+#   print(ybar)
+#   print(s)
+
+  t2 <- nrow(x2)%*%t(ybar-mu0)%*%solve(s)%*%(ybar-mu0)
+  f <- (nrow(x2)-ncol(x2))*t2/ncol(x2)/(nrow(x2)-1)
+  df1=ncol(x2)
+  df2=nrow(x2)-ncol(x2)
+  p <- 1-pf(f, df1, df2, lower.tail = TRUE, log.p = FALSE)
+  result2 <- data.frame(t2=t2,F=f,df1=df1,df2=df2,p.value=p);
+
+  #Final results
+  result <- rbind(result1,result2)
+  colnames(result) <- c("T-Squared","F","df1","df2","p-value")
+  rownames(result) <- c("Ho: Ratios of the means over Mu0=1","Ho: All of the ratios are equal to each other")
   
   cat("\nProfile Analysis for One Sample with Hotelling's T-Square:\n")
   cat("\n")
-  
-  return(ftable)
+  return(result)
 }
 
 
