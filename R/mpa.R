fa0c9829779de1b5ea9749f3541016c2d5f7b1ef
Christopher David Desjardins <cddesjardins@gmail.com>
2015-08-25 17:53:11 +0000

    finished map and ran roxygen

--- R/mpa.R
+++ R/mpa.R
@@ -2,7 +2,7 @@
 #'
 #' Implements the moderated profile analysis approach developed by Davison & Davenport (unpublished)
 #'
-#' The function returns the criterion-related moderated profile analysis described in Davison & Davenport (unpublished). Missing data are presently handled by specifying \code{na.action = "na.omit"}, which performs listwise deletion and \code{na.action = "na.fail"}, the default, which causes the function to fail. The following S3 generic functions are available: \code{summary()},\code{anova()}, \code{print()}, and \code{plot()}. These functions provide a summary of the analysis (namely, R2 and the level and pattern components); perform ANOVA of the R2 for the pattern, the level, and the overall model; provide output similar to \code{lm()}, and plots the pattern effect.
+#' The function returns the criterion-related moderated profile analysis described in Davison & Davenport (unpublished). Missing data are presently handled by specifying \code{na.action = "na.omit"}, which performs listwise deletion and \code{na.action = "na.fail"}, the default, which causes the function to fail. The following S3 generic functions are not yet available but will be in future implementations. \code{summary()},\code{anova()}, \code{print()}, and \code{plot()}. These functions provide a summary of the analysis (namely, R2 and the level and pattern components); perform ANOVA of the R2 for the pattern, the level, and the overall model; provide output similar to \code{lm()}, and plots the pattern effect.
 #' @export
 #' @param formula An object of class \code{\link{formula}} of the form \code{response ~ terms}.
 #' @param data An optional data frame, list or environment containing the variables in the model.
@@ -11,28 +11,17 @@
 #' @param stage2 Should stage 2 be executed regardless of stage 1 outcome? defaults to FALSE.
 #' @return An object of class \code{critpat} is returned, listing the following components:
 #' \itemize{
-#'  \item \code{lvl.comp} - the level component
-#'  \item \code{pat.comp} - the pattern component
-#'  \item \code{b} - the unstandardized regression weights
-#'  \item \code{bstar} - the mean centered regression weights
-#'  \item \code{xc} - the scalar constant times bstar
-#'  \item \code{k} - the scale constant
-#'  \item \code{Covpc} - the pattern effect
-#'  \item \code{Ypred} - the predicted values
-#'  \item \code{r2} - the proportion of variability attributed to the different components
-#'  \item \code{F.table} - the associated F-statistic table
-#'  \item \code{F.statistic} - the F-statistics
-#'  \item \code{df} - the df used in the test
+#'  \item \code{r2_full} - the proportion of variability attributed to the full model
+#'  \item \code{r2_red} - the proportion of variability attributed to the reduced model
+#'  \item \code{ftable} - the associated F-statistic table
+#'  \item \code{F-statistic} - the F-statistics
+#'  \item \code{df} - the dfs used in the test
 #'  \item \code{pvalue} - the p-values for the test}
 #'
 #' @examples
 #' \dontrun{
 #' data(mod_data)
-#' mod <- mpa(dv ~ pred1 * mod + pred2 * mod, data = mod_data)
-#' print(mod)
-#' summary(mod)
-#' plot(mod)
-#' anova(mod)
+#' mod <- mpa(dv ~ pred1 * mod + pred2 * mod, moderator = "mod", data = mod_data)
 #' }
 #'
 #' @references Davison, M., & Davenport, E. (unpublished). Comparing Criterion-Related Patterns of Predictor Variables across Populations Using Moderated Regression.
@@ -67,12 +56,15 @@ mpa <- function(formula, data, moderator, k=100, na.action = "na.fail", stage2 =
     foc <- which(mod_data == reg_names[2])
     x.ref <- dat[ref,-c(1, loc)]
     x.foc <- dat[foc,-c(1, loc)]
+    pat.comp <- x - apply(x,1,mean)
     pat.comp.ref <- matrix(ncol = pred_num, rep(0,length(z)*pred_num))
     pat.comp.foc <- matrix(ncol = pred_num, rep(0,length(z)*pred_num))
+    rownames(pat.comp.ref) <- c(1:dim(pat.comp.ref)[1])
+    rownames(pat.comp.foc) <- c(1:dim(pat.comp.foc)[1])
     pat.comp.ref.t <- x.ref - apply(x.ref,1,mean)
-    pat.comp.ref[which(mod_data == reg_names[1]),] <- pat.comp.ref.t
-    
+    pat.comp.ref[which(mod_data == reg_names[1]),] <- as.matrix(pat.comp.ref.t)
     pat.comp.foc.t <- x.foc - apply(x.foc,1,mean)
+    pat.comp.foc[which(mod_data == reg_names[2]),] <- as.matrix(pat.comp.foc.t)
     
     # Set up the regression weight contrast
     
@@ -94,56 +86,29 @@ mpa <- function(formula, data, moderator, k=100, na.action = "na.fail", stage2 =
     bfocsum <- sum(bfoc_tmp)
     
     V <- length(bref)
-    Covpc.ref <- V*(as.matrix(pat.comp.ref)%*%as.matrix(refstar))
-    Covpc.foc <- V*(as.matrix(pat.comp.foc)%*%as.matrix(focstar))
+    Covpc.ref <- V*(as.matrix(pat.comp)%*%as.matrix(refstar))
+    Covpc.foc <- V*(as.matrix(pat.comp)%*%as.matrix(focstar))
     
     model_data <- data.frame(resp, first = brefsum*Xp, sec = (bfocsum - brefsum)*z*Xp, thr = V*Covpc.ref, four =  (Covpc.foc - Covpc.ref)*V*z, z = z)
     
-    lm_full <- lm(V*as.numeric(z)*(Covpc.foc - Covpc.ref) + as.numeric(z))
-  } else {
+    lm_full <- lm(resp ~ first + sec + thr + four + z, model_data)
+    lm_red <- update(lm_full, .~. -four)
+    r2_full <- summary(lm_full)$r.squared
+    r2_red <- summary(lm_red)$r.squared
+    F <- (r2_full - r2_red)/(1 - r2_full) * (nrow(x) - 2*V - 2)/(V - 1)
+    p.value <- 1 - pf(F, V- 1, nrow(x) - 2*V - 2)
+    ftable <- cbind(F, df1 = V - 1, df2 = nrow(x - 2*V - 2), p.value)
+    colnames(ftable) <- c("F-statistic", "df1", "df2", "p - value")
+    rownames(ftable) <- c("R2full = R2red")
+    cat("# -------- F-table --------  #\n\n")
+    cat(paste("The F-statistic is:",F, "with", V-1, "and", nrow(x) - 2*V - 2, "degrees of freedom.\nThe p-value is:", p.value,"\n"))
+    cat(paste("The R2 squared for the full model is:", r2_full, "and the R2 squared for the reduced model was:", r2_red,"\n"))
+    call<- match.call()
+    output <- list(call=call,r2_full, r2_red,ftable=ftable)
+    #class(output) <- "critpat"
+    #return(output)
+   } else {
     cat("Interaction not significant. Not executing stage 2\n")
     break
+   }
   }
-  
-  #     ypred <- fitted(lm(y ~ 1 + Covpc + Xp, na.action = na.action))
-  #     R2.f <- cor(ypred,y)^2
-  #     R2.pat <- cor(y,as.vector(Covpc))^2 ## pattern effect
-  #     R2.lvl <- cor(y,Xp)^2 ## level effect
-  #     r2 <- rbind(R2.f,R2.pat,R2.lvl)
-  #     colnames(r2) <- "R2"
-  #     rownames(r2) <- c("Full Model","Pattern","Level")
-  #     r2 <- round(r2,digits=6)
-  # 
-  #     full.df <- c(v,N-v-1)
-  #     F.R2.full <- (R2.f*full.df[2])/((1-R2.f)*full.df[1])
-  #     p.value.F.R2.full <- pf(F.R2.full,full.df[1],full.df[2],lower.tail=FALSE)
-  # 
-  #     pat.df <- c(v-1,N-v-1)
-  #     F.R2.pat <- ((R2.f - R2.lvl)*pat.df[2])/((1-R2.f)*pat.df[1])
-  #     p.value.F.R2.pat <- pf(F.R2.pat,pat.df[1],pat.df[2],lower.tail = FALSE)
-  # 
-  #     lvl.df<-c(1,N-v-1)
-  #     F.R2.lvl <- ((R2.f - R2.pat)*lvl.df[2])/((1-R2.f))
-  #     p.value.F.R2.lvl <- pf(F.R2.lvl,lvl.df[1],lvl.df[2],lower.tail=FALSE)
-  # 
-  #     F.R2.pat.only <- ((R2.pat)*pat.df[2])/((1-R2.pat)*pat.df[1])
-  #     p.value.F.R2.pat.only <- pf(F.R2.pat.only,pat.df[1],pat.df[2],lower.tail = FALSE)
-  # 
-  #     F.R2.lvl.only <- ((R2.lvl)*lvl.df[2])/((1-R2.lvl)*lvl.df[1])
-  #     p.value.F.R2.lvl.only <- pf(F.R2.lvl.only,lvl.df[1],lvl.df[2],lower.tail = FALSE)
-  # 
-  #     fvalue <- c(F.R2.full,F.R2.pat.only,F.R2.lvl.only,F.R2.pat,F.R2.lvl)
-  #     df <- rbind(full.df,pat.df,lvl.df,pat.df,lvl.df)
-  #     pvalue <- rbind(p.value.F.R2.full,p.value.F.R2.pat.only,p.value.F.R2.lvl.only,p.value.F.R2.pat,p.value.F.R2.lvl)
-  #     ftable <- cbind(df,fvalue,pvalue)
-  # 
-  #     rownames(ftable) <- c("R2.full = 0 ","R2.pat = 0","R2.lvl = 0","R2.full = R2.lvl","R2.full = R2.pat")
-  #     colnames(ftable) <- c("df1", "df2", "F value", "Pr(>F)")
-  # 
-  #     call<- match.call()
-  #     output <- list(call=call,lvl.comp=Xp,pat.comp=pat.comp,b=regweg,bstar=bstar, xc=xc, k=k, Covpc=Covpc, Ypred=ypred,r2=r2,ftable=ftable)
-  # 
-  #     class(output) <- "critpat"
-  #     return(output)
-  #   }
-}
