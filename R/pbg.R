2f73d7388e008484d4b0b52daa1d1104800df98e
Okan Bulut <okanbulut84@gmail.com>
2017-04-01 16:22:47 -0600

    Updated the flatness test in pbg

--- R/pbg.R
+++ R/pbg.R
@@ -90,23 +90,11 @@ pbg <- function(data, group, original.names=FALSE, profile.plot=FALSE) {
     equal.levels <- summary(fit2)
     
     #Flatness
-    cont2 <- diag(x = -1, k, k)
-    for(i in 1:(k-1)) {cont2[i,(i+1)]=1}
-    cont2 <- cont2[1:(k-1),]
-    allS <- var(z[,1:k]) #overall variance
-    xbar <- apply(z[,1:k],2,mean) #grand mean
-    xbar.dif <- cont2 %*% xbar
-    
-    c.vector <- vector()
-    for (i in 1:g) {
-      c.vector[i] <- nrow(z[z$group==levels(z$group)[i],])
-    }
-    
-    F.val <- (sum(c.vector, na.rm = TRUE))*sum(xbar.dif*(solve(cont2%*%allS%*%t(cont2)) %*% xbar.dif))
-    p.val <- pf(F.val*(sum(c.vector, na.rm = TRUE)-k+1)/(sum(c.vector, na.rm = TRUE)-1)/(k-1), (k-1), (sum(c.vector, na.rm = TRUE)-k+1), 
-             lower.tail=F)
+    t.sqr <- (sum(c.vector, na.rm = TRUE))*t(xbar.dif)%*%solve(summary(fit1, test="Wilks")$SS$Residuals)%*%xbar.dif
+    F.val <- (sum(c.vector, na.rm = TRUE)-g-k+2)/(k-1)*t.sqr
     df1 <- (k-1)
-    df2 <- (sum(c.vector, na.rm = TRUE)-k+1)
+    df2 <- (sum(c.vector, na.rm = TRUE)-g-k+2)
+    p.val <- pf(F.val, df1, df2, lower.tail = FALSE)
     flatness <- data.frame(F.val,df1,df2,p.val)
     names(flatness) <- c("F","df1","df2","p-value")
     
